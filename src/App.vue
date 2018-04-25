dea582191fb409c7118d7f09e3aa648f6f2102ff
Oscar Sitges <oscar.ss.mm@gmail.com>
2018-04-25 12:21:07 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 {{ msg }}
+    h2 Essential Links
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    spinner(v-show="loading") 
+    ul
+      artist(v-for="artist in artists"
+      v-bind:artist="artist")
+</template>
+
+<script>
+import getArtist from './api'
+import Artist from './components/Artist'
+import Spinner from './components/Spinner.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'OscarMusic',
+      artists:[],
+      countries: [
+        {name: 'Argentina', value:'argentina'},
+        {name: 'Colombia', value:'colombia'},
+        {name: 'España', value:'spain'}
+      ],
+      selectedCountry:'argentina',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods: {
+    refreshArtists(){
+      const self = this
+      this.loading= true
+      this.artists=[]
+    getArtist(this.selectedCountry)
+      .then(function (artists){
+        self.loading=false
+        self.artists = artists
+      })
+    }
+  },
+  mounted() {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  margin-top 60px
+
+h1
+  font-weight normal
+  color green
+h2
+  font-style italic
+  color red
+
+ul 
+  list-style-type none
+  padding 0
+
+li
+  align center
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default {
+    appName: 'oscarmusic',
+    apiKey: '88b4209d409303925775fe6a49bdf679',
+    secret: 'd4272e0caeeb41b5add3dc55ebc4cdaa',
+    registeredTo: 'oscarsm13'
+
+}
+
+
+
+/*Application name 	oscarmusic
+API key 	88b4209d409303925775fe6a49bdf679
+Shared secret 	d4272e0caeeb41b5add3dc55ebc4cdaa
+Registered to 	oscarsm13*/
